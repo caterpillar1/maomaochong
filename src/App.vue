6b13c7c1d36f4c454e23f20bac79230a35c418ae
dxn <782871476@qq.com>
2021-07-01 10:40:43 +0800

    feat:CSS样式

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,39 @@
 <template>
   <div id="app">
-
-   <form class="layui-form" action="">
-    <div class="layui-form-item">
-      <label class="layui-form-label">输入框</label>
-      <div class="layui-input-block">
-        <input type="text" name="title" required  lay-verify="required" placeholder="请输入标题" autocomplete="off" class="layui-input">
-      </div>
+    <div class="container">
+      <div class="left">左边</div>
+      <div class="main">中间</div>
+      <div class="right">右边</div>
     </div>
-  </form>
+    <div class="box"></div>
   </div>
 </template>
-
-<style lang="scss">
+<style>
+  /* 1. flex布局 */
+  .container{
+    display:flex;
+    justify-content: center;
+    height: 200px;
+    background: #eee;
+  }
+  .left {
+    width: 200px;
+    background-color: red;
+    height: 100%;
+  }
+  .main {
+    background-color: yellow;
+    flex: 1; /*重点*/
+  }
+  .right {
+    width: 200px;
+    background-color: green;
+  }
+  .box{
+    width:0px;
+    height:0px;
+    border: 50px solid;
+    /*获得红色向右的箭头*/
+    border-color:blue green orange red;
+  }
 </style>
-
-
-
